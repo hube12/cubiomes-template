2a8377e594881889d5a2ea5efd98d1221ddd0c25
Neil <h66r10@gmail.com>
2021-06-30 03:58:21 +0200

    basic setup

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+// check the biome at a block position
+#include "finders.h"
+#include <cstdio>
+
+int main()
+{
+    // Initialize a stack of biome layers that reflects the biome generation of
+    // Minecraft 1.16
+    LayerStack g;
+    setupGenerator(&g, MC_1_16);
+
+    int64_t seed;
+    Pos pos = {0,0}; // block position to be checked
+
+    for (seed = 0; ; seed++)
+    {
+        // Go through the layers in the layer stack and initialize the seed
+        // dependent aspects of the generator.
+        applySeed(&g, seed);
+
+        // To get the biome at single block position we can use getBiomeAtPos().
+        int biomeID = getBiomeAtPos(&g, pos);
+        if (biomeID == jungle_edge)
+            break;
+    }
+
+    printf("Seed %" PRId64 " has a Junge Edge biome at block position "
+           "(%d, %d).\n", seed, pos.x, pos.z);
+
+    return 0;
+}
